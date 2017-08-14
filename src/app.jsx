be5345ac194be3023a68cd9c090d5138ef7814c4
DeshpandeBoy <samiradeshpande10@gmail.com>
2017-08-14 11:55:47 +0530

    updated

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./app.scss";
 
-const App = () => {
-  return <h1>hello</h1>;
-};
+const App = () => <h1>hello</h1>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
